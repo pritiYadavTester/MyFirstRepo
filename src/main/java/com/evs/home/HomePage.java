a96e4a5106006d1c6c0156003470226ee7d8c7ba
priti <pritiy458@gmail.com>
2021-11-10 15:26:25 +0530

    hii its my commit for practicse

--- src/main/java/com/evs/home/HomePage.java
+++ src/main/java/com/evs/home/HomePage.java
@@ -1,9 +1,26 @@
 package com.evs.home;
 
-public class HomePage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+import com.evs.Utility.Utility;
+
+public class HomePage {
+@FindBy(xpath = "//a[text()='Sales']]")
+	private WebElement sale;
 	
 	
 	
 	
+	Utility utill;
+	
+	public  HomePage(Utility utill){
+		 this.utill=utill;
+		 PageFactory.initElements(Utility.getDriver(), this);
+	 }
+	public void sale() {
+		utill.moveToElement(sale);
+		
+	}
 }
--- src/test/java/com/evs/Homepage/TestHomepage.java
+++ src/test/java/com/evs/Homepage/TestHomepage.java
@@ -1,5 +1,23 @@
 package com.evs.Homepage;
 
-public class TestHomepage {
+import org.testng.annotations.Test;
 
+import com.evs.LoginPage.Login;
+import com.evs.home.HomePage;
+import com.evs.testBase.TestBasePage;
+
+public class TestHomepage extends TestBasePage{
+	@Test
+public void homePage() {
+Login log=	new Login(utill);
+HomePage home=	new HomePage(utill);
+	home.sale();
+	
+	
+}
+	
+	
+	
+	
+	
 }
